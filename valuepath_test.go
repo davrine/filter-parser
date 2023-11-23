a89158f9af1268387c51015bea05fc3c124f5d54
Jericho <Jericho.adalin@swiftconnect.io>
2023-11-23 14:04:39 -0500

    Fixed valuepath test

--- valuepath_test.go
+++ valuepath_test.go
@@ -2,7 +2,6 @@ package filter
 
 import (
 	"fmt"
-	"testing"
 )
 
 func ExampleParseValuePath() {
@@ -13,16 +12,4 @@ func ExampleParseValuePath() {
 	// emails[type eq "work"] <nil>
 	// emails[not (type eq "work")] <nil>
 	// emails[type eq "work" and value co "@example.com"] <nil>
-}
-
-func TestValuePath(t *testing.T) {
-	e1 := "emails[type eq \"work\"]"
-	e2 := "emails[not (type eq \"work\")]"
-	e3 := "emails[type eq \"work\" and value co \"example.com\" or value co \"test.com\"]"
-	vp1, _ := ParseValuePath([]byte(e1))
-	vp2, _ := ParseValuePath([]byte(e2))
-	vp3, _ := ParseValuePath([]byte(e3))
-	fmt.Printf("e1: %s", vp1.String())
-	fmt.Printf("e2: %s", vp2.String())
-	fmt.Printf("e3: %s", vp3.String())
 }
